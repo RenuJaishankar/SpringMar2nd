b260d6d0059d3148dce4aab638c0ad3f0be6935e
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-03-04 15:34:08 -0500

    changes in front end form

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -26,3 +26,50 @@ tbody.appendChild(tr)
 //    }))
    // .then(json => JSON.stringify(json))
  //   .then(data => Document.querySelector(".dummy").innerHTML = data)
+ // delete method that is tied to the trashcan icon
+
+ const deleteItem = (id) => {
+  let item = document.querySelector(`.row-${id}`)
+ fetch(`http://localhost:8080/donut/${id}`, {
+     method: 'DELETE',
+ })
+ .then(item.remove())
+ .then(console.log(`this works`))
+ 
+
+}
+
+// update method that will be tied to pencil icon
+
+const updateItem = () => {
+  console.log("update works")
+}
+
+
+
+
+
+const addItem = () => {
+
+let nameInput = document.getElementById("name").value
+let frostedInput = document.getElementById("frosted").value
+let typeInput = document.getElementById("type").value
+
+
+let sampleDonut = {
+      name: nameInput,
+      frosted: frostedInput,
+      donutType: typeInput
+}
+
+  console.log(sampleDonut)
+  fetch(`http://localhost:8080/donut`, {
+      headers: { 
+          'Accept': 'application/json',
+          'Content-Type': 'application/json' 
+      },
+      method: 'POST',
+      body: JSON.stringify(sampleDonut)
+  
+  }).then(console.log("cake posted!"))
+}
